86077359136bd9741fa5b7950788daf9f5de58f7
chelseydenton <dentonchelsey@gmail.com>
2014-02-25 13:56:08 -0600

    test links

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -39,4 +39,16 @@ describe "StaticPages" do
 
     it_should_behave_like "all static pages"
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "FAQ"
+    expect(page).to have_title(full_title('FAQ'))
+    click_link "Blog"
+    expect(page).to have_title(full_title('Blog'))
+    click_link "Bob Loblaw's Law Blog"
+    expect(page).to have_title(full_title('Home'))
+  end
 end
