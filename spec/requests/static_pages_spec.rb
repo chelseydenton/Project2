ea695d7effd0fb2d55de3975323c49330150852e
chelseydenton <dentonchelsey@gmail.com>
2014-02-25 12:57:38 -0600

    messed up something for heroku, trying to fix

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,7 @@ describe "StaticPages" do
   shared_examples_for "all static pages" do
     it { should have_selector('h1', text: heading) }
     it { should have_title(full_title(page_title)) }
-  end
+  end 
 
   describe "Home page" do
     before { visit root_path }
